824e966872ab894dbc22e5ad5656bf06085768dc
Jorge Daniel Garcia Gonzalez <garciagonzalez.jorgedaniel@gmail.com>
2014-10-09 18:30:50 -0500

    jueves 9oct casa

--- js/aplicacion.js
+++ js/aplicacion.js
@@ -1,12 +1,11 @@
 jQuery(document).ready(function() {
 
-    
-    var altoPantalla = $(window).height();
-    var altoBarra = $(".fondo-negro").height();
 
     window.onscroll = function() {
+        var altoPantalla = $(window).height();
         if (window.pageYOffset >= altoPantalla){
             $('.fondo-negro').css({position: 'fixed'});
+            var altoBarra = $(".fondo-negro").height();
             $('.bloque-aboutus').css({"margin-top": altoBarra});
         } else if (window.pageYOffset <= altoPantalla) {
             $('.fondo-negro').css({position: 'relative'});
